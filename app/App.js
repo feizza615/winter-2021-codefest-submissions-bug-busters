18f0798450bd732512b13ee4bc68197c19155937
Feizza Fazilatun <feizza01@gmail.com>
2021-01-06 18:22:16 -0500

    added timer 4

--- app/App.js
+++ app/App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Timer from "./components/Timer"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+
+  const Stack = createStackNavigator({
+    Timer: { screen: Timer },
+  });
+  
+  const App = createAppContainer(Stack);
+  export default App;
+
 
 const styles = StyleSheet.create({
   container: {
