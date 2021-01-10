52580372ac785074510073089279045a98a801b8
Anne Zats <>
2021-01-09 21:52:35 -0500

    working checklist

--- app/components/Checklist.js
+++ app/components/Checklist.js
@@ -2,41 +2,38 @@ import React , {useState, useEffect} from 'react'
 import { StyleSheet, TouchableOpacity, TextInput, Text, View,ScrollView, Button } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
-const CheckBox = ({ selected, onPress, style, textStyle, size = 30, color = '#211f30', text = '', ...props}) => (
-    <TouchableOpacity style={[styles.checkBox, style]} onPress={onPress} {...props}>
-        <Icon
-            size={size}
-            color={color}
-            name={ selected ? 'check-box' : 'check-box-outline-blank'}
-        />
+const CheckBox =()=>{
+      const [selected, setSelected] = useState(false);
+      const changeBoxStatus=()=> setSelected(!selected)
 
-        <Text style={textStyle}> {text} </Text>
-    </TouchableOpacity>
-)
+      return(
+        <TouchableOpacity style={[styles.checkBox]} onPress={changeBoxStatus}>
+            <Icon
+                size={30}
+                color={'#211f30'}
+                name={ selected ? 'check-box' : 'check-box-outline-blank'}
+            />
+            <TextInput placeholder="type here"/>
+        </TouchableOpacity>
+      )
+
+}
 
 export default class Checklist extends React.Component {
     static navigationOptions = {title: 'Checklist'};
-    state = {
-        termsAccepted: false
-    }
-
-    handleCheckBox = () => this.setState({ termsAccepted: !this.state.termsAccepted })
 
     render(){
+      var rows=[];
+      for (var i=0; i<10;i++){
+        rows.push(<CheckBox key={i}/>); //makes ten rows of checkbox lines
+      }
       return(
         <>
-        <CheckBox
-            selected={this.state.termsAccepted}
-            onPress={this.handleCheckBox}
-            text='Input should be here'
-        />
-        <CheckBox
-            selected={this.state.termsAccepted}
-            onPress={this.handleCheckBox}
-            text='Input should be here'
-        />
+        <Text>Things to do:</Text>
+        {rows}
         </>
-    )};
+      )
+    };
 
 }
 
