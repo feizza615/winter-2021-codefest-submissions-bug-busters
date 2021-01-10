8d87a40bd389604e7b615f0645595716ffa862d2
Anne Zats <>
2021-01-10 00:27:12 -0500

    optimized timer

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -4,83 +4,79 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class Timer extends React.Component{
     static navigationOptions = {title: 'Timer'};
-
-    render(){const NewTimer =(props)=>{
-    //if (props.start){ //for connecting button
-    const [timeLeft, setTimeLeft]= useState("Hasn't started"); //initializes timer text
-    var i=0 //counter of number of sessions
-    var then= props.newtime[i]*60*1000+Date.now() //finds end of first timer time (in milliseconds)
-    useEffect(()=>{ //function that repeats every second
-        var x = setInterval(()=>{ //calculates how much time is left
-
-        var now = new Date().getTime();
-        var difference = then-now;
-
-        var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-        setTimeLeft(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
-
-        if (difference<0){ //when timer ends
-            if (i== props.newtime.length-1){
-            clearInterval(x);
-            setTimeLeft("done");
-            }
-            else{
-            i++;
-            then= props.newtime[i]*60*1000+Date.now() //resets timer
-            setTimeLeft("switch what you are doing")
-            //insert push notification here
-            }
-        }
-
-        },1000); //the 1000 is what makes it repeat every second
-        return () => clearInterval(x);
-    },[]);
-
-    return(
-        <Text>{timeLeft}</Text>
-    )
-    }//}//for connecting button
-
-    //creates some text and a button
-    const TimerSetup = (props) => {
-    const [getTime, setTime]= useState(0);
-    return(
-        <>
-        <Text> Timer from now to {props.time} </Text>
-        <Button
-        onPress={()=>{
-            //insert make the button actually start the timer
-            setTime(1);
-            //var start=true;//for connecting button
-
-        }}
-        disabled={Boolean(getTime)}
-        title={getTime ? "happy studying:)" : "Click to start"}
-        />
-        </>
-    );}
-
+    state={
+      buttonclicked: false
+    }
+    render(){
     var newtime1 = new Date("Jan 4, 2021 21:00:00"); //9pm today
-    var timearray=[0.1,0.1,0.1] //change this array to Matthew's
     const { navigate, state } =this.props.navigation;
-    //var start = false; //for connecting button
-    //, start={start}/> //for connecting button (put in <Newtimer>)
-    return (
+    var startTimer = false; //for connecting button
 
+    return (
       <View style={styles.container}>
-        <TimerSetup time={newtime1.toLocaleString()}/>
-        <NewTimer newtime={state.params.listoftimes}/>
+        <Text> Timer from now to {newtime1.toLocaleString()} </Text>
         <Button
-           title= "Go to Checklist Screen"
+          onPress={()=>{
+            this.setState({ buttonclicked: true });
+            startTimer=true;
+          }}
+          disabled={this.state.buttonclicked}
+          title={this.state.buttonclicked ? "happy studying:)" : "Click to start"}
+        />
+        <NewTimer newtime={state.params.listoftimes} startTimer={true}/>
+        <Button
+           title= "Before you start, make a to do list!"
            onPress={() => navigate('Checklist')}
 
        />
         <StatusBar style="auto" />
       </View>
     );}
+}
+
+const NewTimer =(props)=>{
+  var timertext = "Hasn't Started"
+  const [timeLeft, setTimeLeft]= useState(timertext); //initializes timer text
+  var i=0 //counter of number of sessions
+  var seconds= props.newtime[i]*60 //time left on timer in seconds
+  useEffect(()=>{
+      var x = setInterval(()=>{ //function that repeats every second
+        if (props.startTimer) {
+          seconds--;
+          timertext=timeLeftCalculator(seconds);
+        }
+        else{
+          timertext="Paused";
+        }
+
+        if (seconds<0){ //when timer ends
+            if (i== props.newtime.length-1){
+              clearInterval(x);//not sure if this needs to be here
+              timertext="done";
+            }
+            else{
+              i++;
+              seconds= props.newtime[i]*60; //resets timer
+              timertext="switch what you are doing";
+              //insert push notification here
+            }
+        }
+        setTimeLeft(timertext);
+      },1000); //the 1000 is what makes it repeat every second
+    return () => clearInterval(x);
+  },[]);
+  return(
+      <Text>{timeLeft}</Text>
+  )
+}
+
+function timeLeftCalculator(seconds){
+  var days = Math.floor(seconds / ( 60 * 60 * 24));
+  var hours = Math.floor((seconds % (60 * 60 * 24)) / ( 60 * 60));
+  var minutes = Math.floor((seconds % ( 60 * 60)) / 60);
+  var secs = Math.floor(seconds % 60 );
+  var timertext=days + "d " + hours + "h " + minutes + "m " + secs + "s "
+  return timertext;
 }
 
 const styles = StyleSheet.create({
