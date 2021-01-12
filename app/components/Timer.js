c3e645a7ce688bbdce67bc700bfb8492a6642ad2
Anne Zats <>
2021-01-12 12:44:11 -0500

    timer WORKS

--- app/components/Timer.js
+++ app/components/Timer.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import useInterval from '@use-it/interval';
 
 export default class Timer extends React.Component{
     static navigationOptions = {title: 'Timer'};
     render(){
       const { navigate, state } =this.props.navigation;
-      let listoftimes=state.params.listoftimes.map(min=>min*60)
-      let end= Date.now()// +sum*60*1000 //insert func that gets end of array
+      let listoftimes=state.params.listoftimes.map(min=>min*60)//mins to seconds
+      let sum = listoftimes.reduce(
+        ( accumulator, currentValue ) => accumulator + currentValue
+      ,0)
+      let end= Date.now()+sum*1000 //this doesn't work
       return(
         <View style={styles.container}>
         <Text> Study session until {end.toLocaleString()} </Text>
@@ -25,58 +29,49 @@ export default class Timer extends React.Component{
 
 
 const NewTimer =(props)=>{
-  let timertext = "  "
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [timeLeft, setTimeLeft]= useState(timertext); //initializes timer text
+  const [timeAlert, setTimeAlert]= useState("Hasn't Started");
+  const [i, setI]=useState(0) //counter of listoftimes
+  const [seconds,setSeconds]=useState(props.listoftimes[i])
 
+  useInterval(()=>{
+    if (!buttonClicked) return
+    if(seconds>1){ //regular timer operations
+      setSeconds(seconds-1)
+      setTimeAlert((i%2) ? "take a break"  : "do work!")
+      return
+    }
+    if (i=== props.listoftimes.length-1){ //if end of timer
+      setTimeAlert("done")
+      setButtonClicked(false)
+      //maybe clearInterval?
+    }
+    else{ //when switching to next timer
+      setSeconds(props.listoftimes[i+1])
+      setI(i+1)
+    }
 
-  let i=0 //counter of number of sessions
-  let seconds= props.listoftimes[i]//time left on timer in seconds
+  },1000)//makes it run every second
 
-  useEffect(()=>{ let x = setInterval(()=>{ //function that repeats every second
-        if (buttonClicked){
-          if (seconds<0){ //when timer ends
-              if (i== props.listoftimes.length-1){
-                clearInterval(x);//not sure if this needs to be here
-                timertext="done";
-              }
-              else{ //when switching to next chunk of time
-                i++;
-                seconds= props.listoftimes[i]; //resets timer
-                timertext="switch what you are doing";
-                //insert push notification here
-              }
-            }
-          else{ //normal timer running
-            timertext=timeLeftCalculator(seconds);
-          }
-          seconds --
-        }
-        else if (timertext=="Hasn't Started"){
-          timertext=" " //this is easier than making it say "Paused"
-        }
-        setTimeLeft(timertext);
-      },1000); //the 1000 is what makes it repeat every second
-    return () => clearInterval(x);},[buttonClicked]);
-  //{(i%2) ? "take a break"  : "do work!"} //this will work when i is a state
-  return(
+return(
     <>
       <Button
         onPress={()=>{
           setButtonClicked(!buttonClicked);
         }}
-        disabled={buttonClicked}
+        //disabled={buttonClicked}
         title={buttonClicked ? "Pause" : "Start"}
       />
-      <Text>{timeLeft} </Text>
+      <Text>{timeLeftCalculator(seconds)} </Text>
+      <Text>{timeAlert} </Text>
+      <ProgressBar i={i}/>
     </>
   )
 }
 
 const ProgressBar=(props)=>{
-  const [numSections, setNumSections]= useState(0); //initializes timer text
   return(
-    <Text>{numSections} done! </Text>
+    <Text>{props.i} sections done! (baby progress bar)</Text>
   )
 }
 
