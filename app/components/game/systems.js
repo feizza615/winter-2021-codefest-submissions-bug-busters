42746f8277e1b644be91bf32d41d070bfddf7b43
Anne Zats <>
2021-01-24 19:55:16 -0500

    still not working

--- app/components/game/systems.js
+++ app/components/game/systems.js
@@ -14,31 +14,33 @@ const ChangeColor = (entities, { touches }) => {
 
   //should i make a button entity??
   //do some stuff with if you touch that button.....
+  var length=Object.keys(entities).length
   var eligibleArray = []
-  for (var i =1; i<7; i++){//entities.size is not a thing
+  for (var i =1; i<length; i++){
     //console.log("Aaa")//not going here
     t= entities[i]
-    t.color = 'pink'   //makes all the dots back to pink
+    //t.color = 'pink'   //makes all the dots back to pink
+    //^dont want it to do that continuously
     if (t.selected < 2){
       eligibleArray.push(t.id) //adds dots that havent been selected 2x already to array
     }
   }
 
   touches.filter(t => t.type === "press").forEach(t => {
-    console.log(t.event.pageX, t.event.pageY)
-    if (0<=t.event.pageX <3 && 0<=t.event.pageY <3){
+    console.log(t.event.pageX, t.event.pageY) //probably pixels
+    if (0<=t.event.pageX <3 && 0<=t.event.pageY <3){ //why is it going into here?
       //gets random index of eligibleArray
       ran1= getRandomID(eligibleArray)
       //uses id to get one dot entity
-      dotOne = entities[eligibleArray[ran1]]
+      dotOne = entities[2]//[eligibleArray[ran1]]
       //removes that id from the eligible list
       eligibleArray.splice(ran1,1)
       //gets second dot entity
-      dotTwo = entities[eligibleArray[getRandomID(eligibleArray)]]
+      dotTwo = entities[1]//[eligibleArray[getRandomID(eligibleArray)]]
       console.log("aaaa")
 
       //makes these two red
-      dotOne.color = 'red';
+      entities[[eligibleArray[ran1]]].color = 'red';
       dotTwo.color = 'red';
       //adds to the counter
       dotOne.selected = dotOne.selected + 1
