8af81223936ebd7c65d92f45cacb03d990369922
Feizza Fazilatun <feizza01@gmail.com>
2021-01-27 18:21:45 -0500

    questionnaire

--- app/components/Questionnaire.js
+++ app/components/Questionnaire.js
@@ -1,10 +1,229 @@
 
 import React,{useState} from 'react'
-import { Image, ImageBackground, StyleSheet, TouchableOpacity, TextInput, Text, View,ScrollView, Button } from 'react-native';
+import { Image, ImageBackground, StyleSheet, TouchableOpacity, TextInput, Text, View, Button, ScrollView,  } from 'react-native';
 import Timer from './Timer'
 import DropDownPicker from 'react-native-dropdown-picker'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import {Header} from 'react-native-elements'
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
+let customFonts = {
+  'NovaSquare': require('../assets/fonts/NovaSquare-Regular.ttf'),
+};
+export default class Questionnaire extends React.Component{
+  static navigationOptions = {title: 'Questionnaire', headerLeft:()=> null, headerShown: false,};
+  state = {
+   hours: 0,
+   mins: 0,
+   breaks: 0,
+   breakTimes:0,
+   //workTimes:0,
+   number:0,
+   select:true,
+   list:[],
+   fontsLoaded: false,
+}
+async _loadFontsAsync() {
+  await Font.loadAsync(customFonts);
+  this.setState({ fontsLoaded: true });
+}
+
+componentDidMount() {
+  this._loadFontsAsync();
+}
+//submit button after hours and minutes
+//give recommendation and option to use/not use
+
+makeListAuto=()=>{
+ let add30=[25,5]
+ let reg50=[20,5,20,5]
+ let reg60=[20,5,25,10]
+ let end45=[20,5,20]
+ let mins=this.state.mins
+ let hours = this.state.hours
+ let list =[]
+ if(mins==-1){
+   list=[.25,.25,.25,.25]
+ }
+ else if (hours!=0){
+   list = new Array(hours-1).fill(reg60).flat()
+ }
+
+ switch(mins){
+   case 0: list=list.concat([25,10,25]); break;
+   case 5: list=list.concat([25,10,30]); break;
+   case 10: list=list.concat(reg50,[20]) ; break;
+   case 15: list=list.concat(reg50,[25]) ; break;
+   case 20: list=list.concat(reg60,[20]) ; break;
+   case 25: list=list.concat(reg60,[25]) ; break;
+   case 30: list=list.concat(reg60,[30]) ; break;
+   case 35: list=list.concat(reg50,end45) ; break;
+   case 40: list=list.concat(reg50,add30,[20]) ; break;
+   case 45: list=list.concat(reg60,end45) ; break;
+   case 50: list=list.concat(reg60,add30,[20]) ; break;
+   case 55: list=list.concat(reg60,add30,[25]) ; break;
+ }
+
+ if (hours===0){
+   list.shift()//removes the added reg60
+   list.shift()
+   list.shift()
+   list.shift()
+ }
+ this.setState({list:list})
+};
+
+makeListManually =()=>{
+ let mins = 60 * this.state.hours + this.state.mins;
+ let breakTime = this.state.breaks * this.state.breakTimes;
+ let workTime = mins-breakTime;
+
+ let oneWorkTime= workTime/(this.state.breaks+1);
+ //this.setState({workTimes:oneWorkTime})
+
+ let list =[]
+ for(let i=0;i<this.state.breaks*2+1;i++){
+    if(i%2==0){
+       list.push(oneWorkTime)
+    }
+    else{
+       list.push(this.state.breakTimes)
+    }
+
+ }
+ this.setState({list:list})
+};
+
+ changeState =()=>{
+     this.setState({select:!this.state.select});
+ };
+
+
+ renderDropDown=()=>{
+     return(
+       <>
+       <Text style={styles.questions}>How many breaks would you like?</Text>
+       <DropDownPicker
+         items={makeDropdownBreakNum()}
+         style={styles.dropContainer}
+          containerStyle={{ height: 50, width:'70%', paddingLeft:10,backgroundColor:'transparent'}}
+          dropDownStyle={{ backgroundColor:'#38127a', alignContent:'center',alignSelf:'center'}}
+          labelStyle={{
+            fontSize: 20,
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            color: 'white',
+            backgroundColor:'transparent'
+        }}
+           placeholderStyle={{
+            color:'white',
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            backgroundColor:'transparent'
+        }}
+         placeholder=""
+         onChangeItem={
+           (item)=>{this.setState ({
+             breaks: item.value
+         })}}/>
+      <Text style={styles.questions}>How long should each break be?</Text>
+       <DropDownPicker
+         items={makeDropdownBreaks()}
+         style={styles.dropContainer}
+          containerStyle={{height: 50, width:'70%', paddingLeft:10,backgroundColor:'transparent',}}
+          dropDownStyle={{backgroundColor:'#38127a', alignContent:'center',alignSelf:'center'}}
+          labelStyle={{
+            fontSize: 20,
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            color: 'white',
+            backgroundColor:'transparent'
+        }}
+           placeholderStyle={{
+            color:'white',
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            backgroundColor:'transparent'
+        }}
+         placeholder=""
+         onChangeItem={
+           (item)=>{this.setState ({
+             breakTimes: item.value
+
+         })}}/>
+       </>
+     );
+ }
+
+  render(){
+      const { navigate } = this.props.navigation; //props comes from App.js
+
+      if (this.state.fontsLoaded) {return(
+       <>
+         
+
+         <ImageBackground source={require("../assets/background.png")} style={{ resizeMode: 'cover', width: '100%', height: '100%' }}>
+         <Header
+           centerComponent={<Image source={require("../assets/logo.png")} style={{ resizeMode: 'center'}} />}
+           containerStyle={{
+             backgroundColor: 'transparent',
+             justifyContent: 'space-around',
+             width: '100%', height: '15%', borderBottomColor:'transparent'
+           }}
+         />
+         <Text style={styles.questions}>How long would you like to study for?</Text>
+         <DropDownPicker
+           items={makeDropdown()}
+           style={styles.dropContainer}
+           containerStyle={{height: 50, width:'70%', backgroundColor:'transparent', paddingLeft:10, alignContent:'center'}}
+           dropDownStyle={{ backgroundColor:'#38127a', alignContent:'center',alignSelf:'center'}}
+           placeholder=""
+           labelStyle={{
+            fontSize: 20,
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            color: 'white',
+            backgroundColor:'transparent',
+        }}
+           placeholderStyle={{
+            color:'white',
+            fontFamily:'NovaSquare',
+            textAlign: 'center',
+            backgroundColor:'transparent',
+            
+        }}
+           onChangeItem={
+             (item)=>{this.setState ({
+               hours: parseInt(Math.floor(item.value/60)),
+               mins: parseInt(item.value%60)
+           })}}
+         />
+
+         <CheckBox clicks={this.state.number} select={this.state.select} changeState={this.changeState}/>
+         {this.state.select?null:this.renderDropDown()}
+           <View style={{paddingTop:20}}>
+         <TouchableOpacity
+           style = {styles.submitButton}
+           onPress = {() => this.state.select?this.makeListAuto():this.makeListManually()}
+           >
+             <Text style={styles.submitButtonText}>View Breakdown</Text>
+         </TouchableOpacity>
+         </View>
+         <Text> {this.state.list}</Text>
+
+         <Button
+             title= "Go to Timer Screen"
+             onPress={() => navigate('Timer', {listoftimes: this.state.list})}
+         />
+         </ImageBackground>
+       </>
+      )}else {
+        return <AppLoading />;
+      }
+  }
+};
+
 
 const CheckBox =(props)=>{
       const [selected, setSelected] = useState(true);
@@ -12,175 +231,22 @@ const CheckBox =(props)=>{
 
       return(
          <>
-         <View>
-        <TouchableOpacity style={[styles.checkBox]} onPress={changeBoxStatus} >
+         <View style={{paddingTop:20, paddingBottom:20, paddingLeft:10}}>
+        <TouchableOpacity style={{flexDirection:'row',alignItems: "left",
+              justifyContent: "left",
+              textAlign: "center"}} onPress={changeBoxStatus} >
             <Icon
-                size={30}
-                color={'#211f30'}
+
+                size={19}
+                color={'white'}
                 name={ selected ? 'check-box' : 'check-box-outline-blank'}
             />
-            </TouchableOpacity><Text>Enable recommendation for breaks?</Text>
+            <Text style={styles.reccomendation}> Enable Reccommended Breakdown</Text>
+            </TouchableOpacity>
             </View>
             </>
       )
 }
-
-
-export default class Questionnaire extends React.Component{
-   static navigationOptions = {title: 'Questionnaire', headerLeft:()=> null, headerShown: false,};
-   state = {
-    hours: 0,
-    mins: 0,
-    breaks: 0,
-    breakTimes:0,
-    //workTimes:0,
-    number:0,
-    select:true,
-    list:[]
- }
- //submit button after hours and minutes
- //give recommendation and option to use/not use
-
-makeListAuto=()=>{
-  let add30=[25,5]
-  let reg50=[20,5,20,5]
-  let reg60=[20,5,25,10]
-  let end45=[20,5,20]
-  let mins=this.state.mins
-  let hours = this.state.hours
-  let list =[]
-  if(mins==-1){
-    list=[.25,.25,.25,.25]
-  }
-  else if (hours!=0){
-    list = new Array(hours-1).fill(reg60).flat()
-  }
-
-  switch(mins){
-    case 0: list=list.concat([25,10,25]); break;
-    case 5: list=list.concat([25,10,30]); break;
-    case 10: list=list.concat(reg50,[20]) ; break;
-    case 15: list=list.concat(reg50,[25]) ; break;
-    case 20: list=list.concat(reg60,[20]) ; break;
-    case 25: list=list.concat(reg60,[25]) ; break;
-    case 30: list=list.concat(reg60,[30]) ; break;
-    case 35: list=list.concat(reg50,end45) ; break;
-    case 40: list=list.concat(reg50,add30,[20]) ; break;
-    case 45: list=list.concat(reg60,end45) ; break;
-    case 50: list=list.concat(reg60,add30,[20]) ; break;
-    case 55: list=list.concat(reg60,add30,[25]) ; break;
-  }
-
-  if (hours===0){
-    list.shift()//removes the added reg60
-    list.shift()
-    list.shift()
-    list.shift()
-  }
-  this.setState({list:list})
-};
-
-makeListManually =()=>{
-  let mins = 60 * this.state.hours + this.state.mins;
-  let breakTime = this.state.breaks * this.state.breakTimes;
-  let workTime = mins-breakTime;
-
-  let oneWorkTime= workTime/(this.state.breaks+1);
-  //this.setState({workTimes:oneWorkTime})
-
-  let list =[]
-  for(let i=0;i<this.state.breaks*2+1;i++){
-     if(i%2==0){
-        list.push(oneWorkTime)
-     }
-     else{
-        list.push(this.state.breakTimes)
-     }
-
-  }
-  this.setState({list:list})
-};
-
-  changeState =()=>{
-      this.setState({select:!this.state.select});
-  };
-
-
-  renderDropDown=()=>{
-      return(
-        <>
-        
-        <DropDownPicker
-          items={makeDropdownBreakNum()}
-          containerStyle={{height: 40}}
-          dropDownStyle={{marginTop: 2}}
-          placeholder="Select how many breaks you want"
-          onChangeItem={
-            (item)=>{this.setState ({
-              breaks: item.value
-          })}}/>
-        <DropDownPicker
-          items={makeDropdownBreaks()}
-          containerStyle={{height: 40}}
-          dropDownStyle={{marginTop: 2}}
-          placeholder="Select how long each break will be"
-          onChangeItem={
-            (item)=>{this.setState ({
-              breakTimes: item.value
-
-          })}}/>
-        </>
-      );
-  }
-
-   render(){
-       const { navigate } = this.props.navigation; //props comes from App.js
-
-       return(
-        <>
-          
-         <Header
-            centerComponent={<Image source={require("../assets/logo.png")} style={{ resizeMode: 'center'}} />}
-            containerStyle={{
-              backgroundColor: '#a5b7f9',
-              justifyContent: 'space-around',
-              width: '100%', height: '12%'
-            }}
-          />
-            
-          <ImageBackground source={require("../assets/background.png")} style={{ resizeMode: 'cover', width: '100%', height: '100%' }}>
-          <DropDownPicker
-            items={makeDropdown()}
-            containerStyle={{height: 40}}
-            dropDownStyle={{marginTop: 2}}
-            placeholder="Select how long you will be studying"
-            onChangeItem={
-              (item)=>{this.setState ({
-                hours: parseInt(Math.floor(item.value/60)),
-                mins: parseInt(item.value%60)
-            })}}
-          />
-          <CheckBox clicks={this.state.number} select={this.state.select} changeState={this.changeState}/>
-          {this.state.select?null:this.renderDropDown()}
-          <TouchableOpacity
-            style = {styles.submitButton}
-            onPress = {() => this.state.select?this.makeListAuto():this.makeListManually()}
-            >
-            <Text style = {styles.submitButtonText}> Submit </Text>
-          </TouchableOpacity>
-
-          <Text> {this.state.list}</Text>
-
-          <Button
-              title= "Go to Timer Screen"
-              onPress={() => navigate('Timer', {listoftimes: this.state.list})}
-          />
-          </ImageBackground>
-        </>
-       )
-   }
-};
-
 
 
 function makeDropdown(){
@@ -206,8 +272,28 @@ function makeDropdownBreakNum(){
 
 
 const styles = StyleSheet.create({
+  title: {
+    fontFamily:'NovaSquare',
+    color:'white',
+    fontSize:20,
+    fontWeight: 'bold',
+    paddingTop:10,
+    paddingLeft:10
+  },
+    questions: {
+      fontFamily:'NovaSquare',
+      color:'white',
+      fontSize:20,
+      paddingTop:20,
+      paddingLeft:15
+    },
+    reccomendation: {
+      fontFamily:'NovaSquare',
+      color:'white',
+      fontSize:15,
+    },
     container: {
-       paddingTop: 23
+   
     },
     input: {
        margin: 15,
@@ -216,12 +302,32 @@ const styles = StyleSheet.create({
        borderWidth: 1
     },
     submitButton: {
-       backgroundColor: '#7a42f4',
-       padding: 10,
-       margin: 15,
-       height: 40,
+       backgroundColor: '#38127a',
+       height: 60,
+      borderWidth:1,
+      borderColor:'white',
+      borderRadius:15,
+      width:'60%',
+      alignSelf:'center',
+      justifyContent:'center'
     },
     submitButtonText:{
-       color: 'white'
-    }
+      fontFamily:'NovaSquare',
+      color:'white',
+      fontSize:25,
+      justifyContent:'center',
+      textAlign:'center'
+ 
+    },
+    dropContainer:{
+      backgroundColor:'transparent', 
+      borderTopColor:'transparent', 
+      borderLeftColor:'transparent', 
+      borderRightColor:'transparent', 
+      borderBottomWidth:1, 
+      borderBottomColor:"white",
+      borderBottomEndRadius:1,
+      borderBottomStartRadius:1
+
+    },
  });
